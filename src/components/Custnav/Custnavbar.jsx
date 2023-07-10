e7fa99a30ead809b0ca8d73d934f7bf1f4806cc1
maibelline <mayb80@hotmail.fr>
2023-07-10 15:43:29 +0200

    custnav css

--- src/components/Custnav/Custnavbar.jsx
+++ src/components/Custnav/Custnavbar.jsx
@@ -7,26 +7,26 @@ import { FaUser } from 'react-icons/fa';
 import { FaToolbox} from 'react-icons/fa';
 import { RiServiceFill } from 'react-icons/ri';
 import { NavLink} from "react-router-dom";
-// import "./custnavbar.css"
+import "../Custnav/custnavbar.css"
 
 
 export const Custnavbar = () => { 
-  return (
-    <div>
-    <div className="header__socials">
-      <a href="mailto:mayb80@hotmail.fr" target="_blank" rel="noopener noreferrer"><BsEnvelopeFill /></a>
-      <a href="https://www.linkedin.com/in/maibelline-quilliet-41492a20b/" target="_blank" rel="noopener noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/m-quilliet" target="_blank" rel="noopener noreferrer" ><FaGithub /></a>
-    </div>
-        <div className="header__header">
-        <NavLink to="/"><ImHome /></NavLink>
-        <NavLink to="/about">< FaUser/></NavLink>
-        <NavLink to="/tool">< FaToolbox/></NavLink>
-        <NavLink to="/services">< RiServiceFill/></NavLink>
-  
-      </div>
-      </div>
-  )
+    return (
+        <div>
+        <div className="header__socials">
+        <a href="mailto:mayb80@hotmail.fr" target="_blank" rel="noopener noreferrer"><BsEnvelopeFill /></a>
+        <a href="https://www.linkedin.com/in/maibelline-quilliet-41492a20b/" target="_blank" rel="noopener noreferrer" ><BsLinkedin /></a>
+        <a href="https://github.com/m-quilliet" target="_blank" rel="noopener noreferrer" ><FaGithub /></a>
+        </div>
+            <div className="header__header">
+            <NavLink to="/"><ImHome /></NavLink>
+            <NavLink to="/about">< FaUser/></NavLink>
+            <NavLink to="/tool">< FaToolbox/></NavLink>
+            <NavLink to="/services">< RiServiceFill/></NavLink>
+    
+        </div>
+        </div>
+    )
 }
 
 
