a835d1cca6d8ef19437199850d1f00664b106bca
maibelline <mayb80@hotmail.fr>
2023-07-10 15:35:19 +0200

    Custnav jsx

---
+++ src/components/Custnav/Custnavbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { BsEnvelopeFill } from 'react-icons/bs';
+import { ImHome} from 'react-icons/im';
+import { FaUser } from 'react-icons/fa';
+import { FaToolbox} from 'react-icons/fa';
+import { RiServiceFill } from 'react-icons/ri';
+import { NavLink} from "react-router-dom";
+// import "./custnavbar.css"
+
+
+export const Custnavbar = () => { 
+  return (
+    <div>
+    <div className="header__socials">
+      <a href="mailto:mayb80@hotmail.fr" target="_blank" rel="noopener noreferrer"><BsEnvelopeFill /></a>
+      <a href="https://www.linkedin.com/in/maibelline-quilliet-41492a20b/" target="_blank" rel="noopener noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/m-quilliet" target="_blank" rel="noopener noreferrer" ><FaGithub /></a>
+    </div>
+        <div className="header__header">
+        <NavLink to="/"><ImHome /></NavLink>
+        <NavLink to="/about">< FaUser/></NavLink>
+        <NavLink to="/tool">< FaToolbox/></NavLink>
+        <NavLink to="/services">< RiServiceFill/></NavLink>
+  
+      </div>
+      </div>
+  )
+}
+
+
+
+
