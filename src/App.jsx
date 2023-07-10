0f2c790216c6dc396145a09ac58754dfaf7db91c
maibelline <mayb80@hotmail.fr>
2023-07-10 15:51:48 +0200

    All components and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import {Custnavbar} from "./components/Custnav/Custnavbar.jsx";
-import { HashRouter as Router} from 'react-router-dom';
-// import {Header} from './components/Head/Header';
-// import Me from './components/About/Aboutme';
-// import Services from './components/Service/Services';
-// import Tool from './components/Tools/Tool';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {Header} from './components/Head/Header';
+import Me from './components/About/Aboutme';
+import Services from './components/Service/Services';
+import Tool from './components/Tools/Tool';
 
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,12 +16,12 @@ function App() {
   return (
     <Router>       
         <Custnavbar/>
-          {/* <Routes>
-            <Route exact path='/' element={<Header/>} />
-            <Route path='/about' element={<Me/>} />
-            <Route path='/services' element={<Services/>} />
-            <Route path='/tool' element={<Tool/>} />
-          </Routes> */}
+            <Routes>
+              <Route exact path='/' element={<Header/>} />
+              <Route path='/about' element={<Me/>} />
+              <Route path='/services' element={<Services/>} />
+              <Route path='/tool' element={<Tool/>} />
+          </Routes> 
     </Router>    
   );
 }
