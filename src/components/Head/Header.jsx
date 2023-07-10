733883c8bc7f8cd578db90069535de73e5b6ab20
maibelline <mayb80@hotmail.fr>
2023-07-10 18:47:09 +0200

    Header css

--- src/components/Head/Header.jsx
+++ src/components/Head/Header.jsx
@@ -3,25 +3,26 @@ import { Row, Col, Container} from 'react-bootstrap'
 import profil from "../../assets/images/snapchat.png"
 import { Fade } from "react-awesome-reveal"
 import cv from '../../assets/Cv_webdesigner.pdf'
-// import "./header.css";
+import '../Head/header.css'
 
 
 export const Header = () => { 
   return (
     <Fade direction="left">
-      <Container id="Home" className="home p-5 min-vh-100 justify-content-center align-items-center">
-        <Row>
-          <Col className="catch-phrase m-4">
+      <Container id="Home" className="home min-vh-100">
+        <Row >
+          <Col className="catch-phrase m-4 p-4">
             <h1>
-              Hi , I am Maibelline <br /> <p>Développeuse Front-end | Webdesigner</p>
+              Hi , I am Maibelline <br /> <span>Développeuse Front-end | Webdesigner</span>
             </h1>
             <p>
-              Développeuse web et web mobile (bac+2) diplômée depuis juin 2022.
-              Passionnée par l'Ui Design, je suis à la recherche d'un contrat de professionnalisation
-              pour le titre professionnel de Webdesigner afin de me spécialiser dans tous les aspects du développement et de l'intégration front-end.
-            </p> 
-            <br/>
-            <p>Je serais ravie d'intégrer une équipe de professionnels passionnés et passionnants pour m'accompagner dans ce projet.
+              Développeuse web et web mobile (bac+2) diplômée depuis juin 2022.<br/>
+              Passionnée par l'Ui Design, je suis à la recherche 
+              d'un contrat de professionnalisation<br/>
+              
+              pour le titre professionnel de Webdesigner afin de me spécialiser dans <br/> tous les aspects du développement et de l'intégration front-end.<br/>
+            
+            Je serais ravie d'intégrer une équipe de professionnels passionnés et<br/> passionnants pour m'accompagner dans ce projet.
             </p>
             <Col className="buttons">
             <a className="a" href={ cv } download><button className="cv" type="submit">Telecharger mon CV</button></a>
