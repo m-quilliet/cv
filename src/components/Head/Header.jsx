ea8bf8085aab32bad8fdb2bd8f06c5c327ac03c2
maibelline <mayb80@hotmail.fr>
2023-07-10 16:05:59 +0200

    Header.jsx

--- src/components/Head/Header.jsx
+++ src/components/Head/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Container} from 'react-bootstrap'
+import profil from "../../assets/images/snapchat.png"
+import { Fade } from "react-awesome-reveal"
+import cv from '../../assets/Cv-WebDesigner.pdf'
+// import "./header.css";
 
-function Header() {
+
+export const Header = () => { 
   return (
-    <div>Header</div>
-  )
+    <Fade direction="left">
+      <Container id="Home" className="home p-5 min-vh-100 justify-content-center align-items-center">
+        <Row>
+          <Col className="catch-phrase m-4">
+            <h1>
+              Hi , I am Maibelline <br /> <p>Développeuse Front-end | Webdesigner</p>
+            </h1>
+            <p>
+              Développeuse web et web mobile (bac+2) diplômée depuis juin 2022.
+              Passionnée par l'Ui Design, je suis à la recherche d'un contrat de professionnalisation
+              pour le titre professionnel de Webdesigner afin de me spécialiser dans tous les aspects du développement et de l'intégration front-end.
+            </p> 
+            <br/>
+            <p>Je serais ravie d'intégrer une équipe de professionnels passionnés et passionnants pour m'accompagner dans ce projet.
+            </p>
+            <Col className="buttons">
+            <a className="a" href={ cv } download><button className="cv" type="submit">Telecharger mon CV</button></a>
+            </Col>
+          </Col>
+        </Row>
+      <Row>
+        <Col  className="profil">
+          <img src={profil} height='500' className="maibelline" alt="maibelline"/>
+        </Col> 
+      </Row>
+    </Container>
+    </Fade>
+  );
 }
-
-export default Header
