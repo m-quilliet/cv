d80092d337cd7cad16b737959198d5678d6be38b
maibelline <mayb80@hotmail.fr>
2023-07-12 12:00:57 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Custnavbar} from "./components/Custnav/Custnavbar.jsx";
 import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import {Header} from './components/Head/Header.jsx';
 import Me from './components/About/Aboutme.jsx';
-import {Services} from './components/Service/Services.jsx';
+// import {Services} from './components/Service/Services.jsx';
 import {Tool} from './components/Tools/Tool.jsx';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
             <Routes>
               <Route exact path='/' element={<Header/>} />
               <Route path='/about' element={<Me/>} />
-              <Route path='/services' element={<Services/>} />
+              {/* <Route path='/services' element={<Services/>} /> */}
               <Route path='/tool' element={<Tool/>} />
           </Routes> 
     </Router>    
