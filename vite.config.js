33047524068b8880d8a04fd0e49645df79cb5c11
maibelline <mayb80@hotmail.fr>
2023-07-16 09:45:48 +0200

    m

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
-import  {  ViteMinifyPlugin  } from 'vite-plugin-minify'
+import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: false,
-  },
-  ViteMinifyPlugin :[( { } )], 
+  plugins:  [react()][viteCompression()],
   base: "/cv/"
 })
