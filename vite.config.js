76ad9d05b6d372c39689ab5afba528458953f33c
maibelline <mayb80@hotmail.fr>
2023-07-18 22:49:43 +0200

    b

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins:  [react()][viteCompression()],
-  base: "/cv/"
-})
+export default () => {
+  return {
+    plugins: [viteCompression()],
+    base: '/cv/'
+  };
+};
